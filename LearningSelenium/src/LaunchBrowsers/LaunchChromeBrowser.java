a3b32288d95b6ac8b01f83e78103d1e2d9f20422
Nawaz9473 <109750538+Nawaz9473@users.noreply.github.com>
2025-10-29 16:17:41 +0530

    commit 1

---
+++ LearningSelenium/src/LaunchBrowsers/LaunchChromeBrowser.java
@@ -0,0 +1,18 @@
+package LaunchBrowsers;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchChromeBrowser {
+	
+	public static void main(String[] args)
+	{
+	   //ChromeDriver driver = new ChromeDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
+		EdgeDriver driver = new EdgeDriver();
+		
+	}
+}
+	
+ 
